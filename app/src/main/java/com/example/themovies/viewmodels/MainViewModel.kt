83aa68c7c358e439201ee782d9408c333b624e3d
Yana Lysa <lysayana95@gmail.com>
2023-01-29 00:00:38 +0200

    feat: MainViewModel setup

---
+++ app/src/main/java/com/example/themovies/viewmodels/MainViewModel.kt
@@ -0,0 +1,39 @@
+package com.example.themovies.viewmodels
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MainViewModel : ViewModel() {
+
+    companion object {
+        const val TAG = "MainViewModel"
+    }
+
+    private val _uiState = MutableStateFlow(MainUIState())
+    val uiState: StateFlow<MainUIState> = _uiState.asStateFlow()
+
+
+    private fun showLoading(value: Boolean) {
+        _uiState.update {
+            it.copy(isLoading = value)
+        }
+    }
+
+
+    fun onErrorMessageShown() {
+        _uiState.update {
+            it.copy(errorMsg = null)
+        }
+    }
+
+
+    data class MainUIState(
+        val isListLoading: Boolean = false,
+        val isDataLoading: Boolean = false,
+        val isLoading: Boolean = false,
+        val errorMsg: String? = null
+    )
+}
