3c299bdf14c8a2555b3e6685647ddb3d11fcb693
Yana Lysa <lysayana95@gmail.com>
2023-01-28 23:34:17 +0200

    feat: create a DetailFragment layout

--- app/src/main/java/com/example/themovies/ui/DetailFragment.kt
+++ app/src/main/java/com/example/themovies/ui/DetailFragment.kt
@@ -1,11 +1,9 @@
 package com.example.themovies.ui
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.themovies.R
 import com.example.themovies.databinding.FragmentDetailBinding
 
 
