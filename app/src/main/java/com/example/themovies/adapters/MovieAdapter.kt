260b583c678586f206ff2de245b2fc69170fe591
Yana Lysa <lysayana95@gmail.com>
2023-01-29 16:12:48 +0200

    feat: add logic for the MovieAdapter.kt

---
+++ app/src/main/java/com/example/themovies/adapters/MovieAdapter.kt
@@ -0,0 +1,57 @@
+package com.example.themovies.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.themovies.R
+import com.example.themovies.databinding.ItemMovieBinding
+import com.example.themovies.model.Movie
+
+class MovieAdapter(
+    private val movies: List<Movie>
+) : RecyclerView.Adapter<MovieAdapter.MovieViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): MovieViewHolder {
+        val v = LayoutInflater.from(parent.context)
+            .inflate(R.layout.item_movie, parent, false)
+        return MovieViewHolder(v)
+
+    }
+
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        holder.bind(movies.get(position))
+    }
+
+    override fun getItemCount(): Int = movies.size
+
+
+    inner class MovieViewHolder(item: View) :
+        RecyclerView.ViewHolder(item) {
+
+        private val binding = ItemMovieBinding.bind(item)
+
+        fun bind(movie: Movie?) {
+            binding.movieTitle.text = movie?.title
+            binding.movieReleaseDate.text = movie?.releaseDate
+
+        }
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
