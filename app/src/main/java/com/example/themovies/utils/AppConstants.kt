b0c988c7a5f47bf225cf42e117248213476a713f
Yana Lysa <lysayana95@gmail.com>
2023-01-29 16:37:35 +0200

    build: add Glide libraries

--- app/src/main/java/com/example/themovies/utils/AppConstants.kt
+++ app/src/main/java/com/example/themovies/utils/AppConstants.kt
@@ -2,3 +2,4 @@ package com.example.themovies.utils
 
 const val BASE_URL = "https://api.themoviedb.org"
 const val API_KEY = "b474ea5ddda74d655d0ff02bb056f680"
+const val IMAGE_BASE = "https://image.tmdb.org/t/p/w500/"
