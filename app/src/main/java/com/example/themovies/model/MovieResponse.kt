075c0a560c86c7b94c455be232bcb2f7906f20ed
Yana Lysa <lysayana95@gmail.com>
2023-01-29 14:54:59 +0200

    feat: create a MovieResponse.kt

---
+++ app/src/main/java/com/example/themovies/model/MovieResponse.kt
@@ -0,0 +1,14 @@
+package com.example.themovies.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class MovieResponse(
+    @SerializedName("result")
+    val movies: List<Movie>
+) : Parcelable {
+    constructor() : this(mutableListOf())
+
+}
