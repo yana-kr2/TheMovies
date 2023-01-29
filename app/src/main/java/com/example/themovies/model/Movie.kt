a94cb7906fa344687ff77985fbb5e7c22ea9a7b6
Yana Lysa <lysayana95@gmail.com>
2023-01-29 23:07:19 +0200

    fix: Movie model fix

--- app/src/main/java/com/example/themovies/model/Movie.kt
+++ app/src/main/java/com/example/themovies/model/Movie.kt
@@ -1,24 +1,13 @@
 package com.example.themovies.model
 
-import android.os.Parcelable
+
 import com.google.gson.annotations.SerializedName
-import kotlinx.android.parcel.Parcelize
 
-@Parcelize
 data class Movie(
-    @SerializedName("id")
-    val id: String?,
-    @SerializedName("title")
-    val title: String?,
-    @SerializedName("poster_path")
-    val poster:String?,
-    @SerializedName("release_date")
-    val releaseDate: String?,
-    @SerializedName("overview")
-    val overview: String?,
-//    @Parcelize("rate")
-//    val rate: Long?
 
-) : Parcelable {
-    constructor() : this("","","","","")
-}
+    val id: Int,
+    val image: Image,
+    val name: String,
+
+
+    )
