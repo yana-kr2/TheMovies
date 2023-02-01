99cbf27c5def3ea852fc44a4b3fe95c777fd3f4d
Yana Lysa <lysayana95@gmail.com>
2023-02-01 07:36:25 +0200

    feat: add constants for tmbd Api

--- app/src/main/java/com/example/themovies/utils/AppConstants.kt
+++ app/src/main/java/com/example/themovies/utils/AppConstants.kt
@@ -2,10 +2,10 @@ package com.example.themovies.utils
 
 
 object AppConstants {
-//    const val BASE_URL = "https://api.themoviedb.org"
+    const val BASE_URL_TMDB = "https://api.themoviedb.org"
     const val BASE_URL = "https://api.tvmaze.com/"
     const val END_POINT = "shows"
-//    const val END_POINT = "/3/movie/top_rated?api_key="
+    const val END_POINT_TMDB = "/3/movie/top_rated?api_key="
     const val API_KEY = "b474ea5ddda74d655d0ff02bb056f680"
     const val IMAGE_BASE = "https://image.tmdb.org/t/p/w500/"
 }
