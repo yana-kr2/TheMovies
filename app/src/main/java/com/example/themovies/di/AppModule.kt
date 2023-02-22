b5863a87fc62621dda5dbb435ea48c06d14c205c
Yana Lysa <lysayana95@gmail.com>
2023-02-22 21:17:53 +0200

    feat: setup OkHttp client

--- app/src/main/java/com/example/themovies/di/AppModule.kt
+++ app/src/main/java/com/example/themovies/di/AppModule.kt
@@ -6,6 +6,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
@@ -19,12 +21,23 @@ object AppModule {
     @Provides
     fun provideBaseUrl() = AppConstants.BASE_URL
 
+
+    @Provides
+    fun getOkHttpClient (): OkHttpClient = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
+
+
+
     @Provides
     @Singleton
     fun provideRetrofitInstance(BASE_URL: String?): MovieApiInterface =
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
+            .client(getOkHttpClient())
             .build()
             .create(MovieApiInterface::class.java)
 }
